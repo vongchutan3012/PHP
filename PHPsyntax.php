903a348b027e3e65b4de5780d7851f66712257b8
chutan3012 <123055159+chutan3012@users.noreply.github.com>
2023-11-11 21:46:48 +0700

    Test commit 11/11

---
+++ PHPsyntax.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html> 
+<meta charset="UTF-8">
+<html>
+    <body>
+        <h1>Đây là PHPsyntax kết hợp với HTML</h1>
+        <?php
+        echo "Hello world";
+        ?>
+    </body>
+</html>
